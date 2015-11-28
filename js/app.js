3183ae13fdb38beb0e6ed7f5958cac79e50a8d62
Benjamin <benjamin.j.arias@gmail.com>
2015-11-28 14:54:42 +0100

    fixed the google is not defined error

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 // --------- MODEL ---------
 // all the data used to place the markers
+
 var places = [
 	{
 		name: 'Berghain Club',
@@ -41,254 +42,252 @@ var places = [
 
 // --------- GOOGLE MAPS --------
 
-var googleMap = {
-	map: {},
-	infoWindow: new google.maps.InfoWindow({maxWidth: 150}), // reusable info window
-	options: {
-		center: { lat: 52.522650, lng: 13.413215},
-		zoom: 13,
-		mapTypeId: google.maps.MapTypeId.HYBRID
-	},
-	infoWindowContent: '<div class="info-window"><div class="window-title">%title%</div><div style={float: right}>%link%</div></div>' ,
-	init: function(vm) {
-		if (typeof(google) !== 'undefined'){
-			googleMap.map = new google.maps.Map(document.getElementById('map'), googleMap.options);
-			// show markers in time with the map loaded
-			if (vm.initialized && !vm.hasMarkers) vm.showMarkers();
-		} else {
-			alert("Failed to load Google map, please reload.");
+var setupMap = function() {
+	var googleMap = {
+		map: {},
+		infoWindow: new google.maps.InfoWindow({maxWidth: 150}), // reusable info window
+		options: {
+			center: { lat: 52.522650, lng: 13.413215},
+			zoom: 13,
+			mapTypeId: google.maps.MapTypeId.HYBRID
+		},
+		infoWindowContent: '<div class="info-window"><div class="window-title">%title%</div><div style={float: right}>%link%</div></div>' ,
+		init: function() {
+			if (typeof(google) !== 'undefined'){
+				googleMap.map = new google.maps.Map(document.getElementById('map'), googleMap.options);
+				// show markers in time with the map loaded
+				if (vm.initialized && !vm.hasMarkers) vm.showMarkers();
+			} else {
+				alert("Failed to load Google map, please reload.");
+			}
 		}
-	}
-};
+	};
 
-//---------- PLACE OBJECT --------
+	//---------- PLACE OBJECT --------
 
-var Place = function(data, viewmodel) {
-	var self = this;
-	this.name = ko.observable(data.name);
-	this.tags = ko.observableArray(data.tags);
-	this.lat = ko.observable(data.lat);
-	this.lng = ko.observable(data.lng);
-	this.label= data.label;
+	var Place = function(data, viewmodel) {
+		var self = this;
+		this.name = ko.observable(data.name);
+		this.tags = ko.observableArray(data.tags);
+		this.lat = ko.observable(data.lat);
+		this.lng = ko.observable(data.lng);
+		this.label= data.label;
 
-	// google maps marker
-	var marker = new google.maps.Marker({
-		position: new google.maps.LatLng(data.lat, data.lng),
-		animation: google.maps.Animation.DROP
-	});
-
-	// click handler the markers
-	google.maps.event.addListener(marker, 'click', function() {
-		viewmodel.showPlace(self);
-	});
-	this.marker = marker;
-};
-
-// --------- FILTER OBJECT -------
-
-var Filter = function(data) {
-	this.name = ko.observable(data.name);
-	this.on = ko.observable(true);
-};
-
-
-//-------- VIEW MODEL ----------
-
-var ViewModel = function() {
-	var self = this;
-	self.searchFilter = ko.observable('');
-	self.currentPlace = ko.observable();
-	self.hasMarkers = false;
-	self.initialized = false;
-	self.connectionError = ko.observable(false);
-
-	// --------- INIT ---------
-
-	self.init = function() {
-		var tempTagArr = [];
-		var tempFilterArr = [];
-
-		// create container for places
-		self.placeList = ko.observableArray([]);
-
-		// loop through places array and convert to ko object
-		places.forEach(function(place) {
-			self.placeList.push(new Place(place, self));
-
-			// loop through tags for each place and add to self.filters
-			place.tags.forEach(function(tag){
-				// if current tag is not already a filter, add to self.filters
-				if (tempTagArr.indexOf(tag) < 0) {
-					tempTagArr.push(tag);
-				}
-			});// end tag loop
-		});// end place loop
-
-		// loop through tags and make filter objects from them
-		tempTagArr.forEach(function(tag){
-			tempFilterArr.push(new Filter({name: tag}));
+		// google maps marker
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(data.lat, data.lng),
+			animation: google.maps.Animation.DROP
 		});
 
-		// set filters based on temporary array
-		// this has performance benefits over pushing items one at a time
-		self.filters = ko.observableArray(tempFilterArr);
+		// click handler the markers
+		google.maps.event.addListener(marker, 'click', function() {
+			viewmodel.showPlace(self);
+		});
+		this.marker = marker;
+	};
 
-		// array of filters currently applied
-		self.currentFilters = ko.computed(function() {
-			var tempCurrentFilters = [];
+	// --------- FILTER OBJECT -------
 
-			// loop through filters and get all filters that are on
-			ko.utils.arrayForEach(self.filters(), function(filter){
-				if (filter.on()) tempCurrentFilters.push(filter.name());
+	var Filter = function(data) {
+		this.name = ko.observable(data.name);
+		this.on = ko.observable(true);
+	};
+
+
+	//-------- VIEW MODEL ----------
+
+	var ViewModel = function() {
+		var self = this;
+		self.searchFilter = ko.observable('');
+		self.currentPlace = ko.observable();
+		self.hasMarkers = false;
+		self.initialized = false;
+		self.connectionError = ko.observable(false);
+
+		// --------- INIT ---------
+
+		self.init = function() {
+			var tempTagArr = [];
+			var tempFilterArr = [];
+
+			// create container for places
+			self.placeList = ko.observableArray([]);
+
+			// loop through places array and convert to ko object
+			places.forEach(function(place) {
+				self.placeList.push(new Place(place, self));
+
+				// loop through tags for each place and add to self.filters
+				place.tags.forEach(function(tag){
+					// if current tag is not already a filter, add to self.filters
+					if (tempTagArr.indexOf(tag) < 0) {
+						tempTagArr.push(tag);
+					}
+				});// end tag loop
+			});// end place loop
+
+			// loop through tags and make filter objects from them
+			tempTagArr.forEach(function(tag){
+				tempFilterArr.push(new Filter({name: tag}));
 			});
 
-			return tempCurrentFilters;
-		});
+			// set filters based on temporary array
+			// this has performance benefits over pushing items one at a time
+			self.filters = ko.observableArray(tempFilterArr);
 
-		// array of places to be shown based on currentFilters
-		self.filteredPlaces = ko.computed(function() {
-			var tempPlaces = ko.observableArray([]);
-			var returnPlaces = ko.observableArray([]);
+			// array of filters currently applied
+			self.currentFilters = ko.computed(function() {
+				var tempCurrentFilters = [];
 
-			// apply filter
-			ko.utils.arrayForEach(self.placeList(), function(place){
-				var placeTags = place.tags();
-
-				// loop through all tags for a place and
-				// determine if any are also a currently applied filter
-				var intersections = placeTags.filter(function(tag){
-					return self.currentFilters().indexOf(tag) != -1;
+				// loop through filters and get all filters that are on
+				ko.utils.arrayForEach(self.filters(), function(filter){
+					if (filter.on()) tempCurrentFilters.push(filter.name());
 				});
 
-				// if one or more tags for a place are in a filter, add it
-				if (intersections.length > 0) tempPlaces.push(place);
+				return tempCurrentFilters;
 			});
 
-			var tempSearchFilter = self.searchFilter().toLowerCase();
+			// array of places to be shown based on currentFilters
+			self.filteredPlaces = ko.computed(function() {
+				var tempPlaces = ko.observableArray([]);
+				var returnPlaces = ko.observableArray([]);
 
-			// if there is no additional text to search for, return filtered places
-			if (!tempSearchFilter){
-				returnPlaces = tempPlaces();
-			}
-			// if user is also searching via text box, apply text filter
-			else{
-				returnPlaces = ko.utils.arrayFilter(tempPlaces(), function(place) {
-		        	return place.name().toLowerCase().indexOf(tempSearchFilter) !== -1;
-		        })
-			};
+				// apply filter
+				ko.utils.arrayForEach(self.placeList(), function(place){
+					var placeTags = place.tags();
 
-			// hide/show correct markers based on list of current places
-			self.filterMarkers(returnPlaces);
-			return returnPlaces;
-		});
+					// loop through all tags for a place and
+					// determine if any are also a currently applied filter
+					var intersections = placeTags.filter(function(tag){
+						return self.currentFilters().indexOf(tag) != -1;
+					});
 
-		// if no markers have been shown, show them
-		if (!self.hasMarkers) self.showMarkers();
-		self.initialized = true;
+					// if one or more tags for a place are in a filter, add it
+					if (intersections.length > 0) tempPlaces.push(place);
+				});
+
+				var tempSearchFilter = self.searchFilter().toLowerCase();
+
+				// if there is no additional text to search for, return filtered places
+				if (!tempSearchFilter){
+					returnPlaces = tempPlaces();
+				}
+				// if user is also searching via text box, apply text filter
+				else{
+					returnPlaces = ko.utils.arrayFilter(tempPlaces(), function(place) {
+			        	return place.name().toLowerCase().indexOf(tempSearchFilter) !== -1;
+			        })
+				};
+
+				// hide/show correct markers based on list of current places
+				self.filterMarkers(returnPlaces);
+				return returnPlaces;
+			});
+
+			// if no markers have been shown, show them
+			if (!self.hasMarkers) self.showMarkers();
+			self.initialized = true;
+		};
+
+		//---------- FUNCTIONS --------
+		var findFirstImage = function(images){
+			var result = $(images).filter(function(index, element){
+				return element.toLowerCase().match(/\.(png|jpg|jpeg|gif)$/);
+				}).first();
+			return result.length > 0 ? result[0] : null;
+		};
+			var getFirstImage = function(title, callback) {
+		        $.ajax({
+		        	type: "GET",
+		        	url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=images&section=0&page="+title+"&callback=?",
+		        	contentType: "application/json; charset=utf-8",
+		            dataType: "json",
+		            success: function (data, textStatus, jqXHR) {
+		                var image = findFirstImage(data.parse.images);
+		                if (image){
+		                	callback(image);
+		                }
+		            },
+		            error: function(){
+		            	alert("Failed to load Image please reload the page.");
+		            }
+		        });
+		    };
+
+	        var getImageUrl = function(name, callback) {
+		        $.ajax({
+		            type: "GET",
+		            url: "https://en.wikipedia.org/w/api.php?action=query&format=json&titles=Image:"+name+"&prop=imageinfo&iiprop=url&iiurlwidth=512&callback=?",
+		            contentType: "application/json; charset=utf-8",
+		            dataType: "json",
+		            success: function (data, textStatus, jqXHR) {
+		                var url = data.query.pages["-1"].imageinfo[0].thumburl;
+		                callback(url);
+		            },
+		            error: function(){
+		            	alert("Failed to load URL please reload the page.");
+		            }
+		        });
+		    };
+
+		// shows/hides correct map markers
+		self.filterMarkers = function(filteredPlaces) {
+			ko.utils.arrayForEach(self.placeList(), function(place){
+				if (filteredPlaces.indexOf(place) === -1) {
+					place.marker.setVisible(false);
+				}
+				else{
+					place.marker.setVisible(true);
+				}
+			});
+		};
+
+		// turns filter on or off
+		// called when filter is clicked in view
+		self.toggleFilter = function(filter) {
+			filter.on(!filter.on());
+		};
+
+		// show the currently selected place
+		// called when list item or map marker is clicked
+		self.showPlace = function(place) {
+			// set info window content and show it
+
+			var wikiLink;
+
+	        getFirstImage(place.label, function(name) {
+		        getImageUrl(name, function(url) {
+		            wikiLink = "<image class = 'info-image'src=\"" + url + "\"/>";
+		            googleMap.infoWindow.setContent(googleMap.infoWindowContent.replace('%title%', place.name()).replace('%link%', wikiLink));
+			    	googleMap.infoWindow.open(googleMap.map, place.marker);
+		            });
+		        });
+
+			// makes marker Bounce when selected
+			place.marker.setAnimation(google.maps.Animation.BOUNCE);
+
+			setTimeout(function(){
+				place.marker.setAnimation(null);
+			}, 2100);
+
+			// reset error status
+			self.connectionError(false);
+		};
+
+		// show marker for each place
+		self.showMarkers = function() {
+			ko.utils.arrayForEach(self.placeList(), function(place){
+				place.marker.setMap(googleMap.map);
+			});
+			self.hasMarkers = true;
+		};
 	};
 
-	//---------- FUNCTIONS --------
-	var findFirstImage = function(images){
-		var result = $(images).filter(function(index, element){
-			return element.toLowerCase().match(/\.(png|jpg|jpeg|gif)$/);
-			}).first();
-		return result.length > 0 ? result[0] : null;
-	};
-		var getFirstImage = function(title, callback) {
-	        $.ajax({
-	        	type: "GET",
-	        	url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=images&section=0&page="+title+"&callback=?",
-	        	contentType: "application/json; charset=utf-8",
-	            dataType: "json",
-	            success: function (data, textStatus, jqXHR) {
-	                var image = findFirstImage(data.parse.images);
-	                if (image){
-	                	callback(image);
-	                }
-	            },
-	            error: function(){
-	            	alert("Failed to load Image please reload the page.");
-	            }
-	        });
-	    };
+	//---------- SETUP ----------
 
-        var getImageUrl = function(name, callback) {
-	        $.ajax({
-	            type: "GET",
-	            url: "https://en.wikipedia.org/w/api.php?action=query&format=json&titles=Image:"+name+"&prop=imageinfo&iiprop=url&iiurlwidth=512&callback=?",
-	            contentType: "application/json; charset=utf-8",
-	            dataType: "json",
-	            success: function (data, textStatus, jqXHR) {
-	                var url = data.query.pages["-1"].imageinfo[0].thumburl;
-	                callback(url);
-	            },
-	            error: function(){
-	            	alert("Failed to load URL please reload the page.");
-	            }
-	        });
-	    };
+	// empty view model
+	var vm = new ViewModel();
 
-	// shows/hides correct map markers
-	self.filterMarkers = function(filteredPlaces) {
-		ko.utils.arrayForEach(self.placeList(), function(place){
-			if (filteredPlaces.indexOf(place) === -1) {
-				place.marker.setVisible(false);
-			}
-			else{
-				place.marker.setVisible(true);
-			}
-		});
-	};
-
-	// turns filter on or off
-	// called when filter is clicked in view
-	self.toggleFilter = function(filter) {
-		filter.on(!filter.on());
-	};
-
-	// show the currently selected place
-	// called when list item or map marker is clicked
-	self.showPlace = function(place) {
-		// set info window content and show it
-
-		var wikiLink;
-
-        getFirstImage(place.label, function(name) {
-	        getImageUrl(name, function(url) {
-	            wikiLink = "<image class = 'info-image'src=\"" + url + "\"/>";
-	            googleMap.infoWindow.setContent(googleMap.infoWindowContent.replace('%title%', place.name()).replace('%link%', wikiLink));
-		    	googleMap.infoWindow.open(googleMap.map, place.marker);
-	            });
-	        });
-
-		// makes marker Bounce when selected
-		place.marker.setAnimation(google.maps.Animation.BOUNCE);
-
-		setTimeout(function(){
-			place.marker.setAnimation(null);
-		}, 2100);
-
-		// reset error status
-		self.connectionError(false);
-	};
-
-	// show marker for each place
-	self.showMarkers = function() {
-		ko.utils.arrayForEach(self.placeList(), function(place){
-			place.marker.setMap(googleMap.map);
-		});
-		self.hasMarkers = true;
-	};
-};
-
-
-//---------- SETUP ----------
-
-// empty view model
-var vm = new ViewModel();
-
-// listener for view model initialization
-var domReady = function(){
 	if (typeof(google) !== 'undefined'){
 		// listener for google map initialization
 		google.maps.event.addDomListener(window, 'load', googleMap.init(vm));
@@ -300,33 +299,8 @@ var domReady = function(){
 			google.maps.event.trigger(googleMap.map, 'resize');
 			googleMap.map.setCenter(googleMap.options.center);
 		});
-		
+
 	} else {
 		alert("Failed to load Google Maps, please reload the page.");
 	}
-	
-};
-
-// used in case of Mozilla, Opera, Webkit 
-if ( document.addEventListener ) {
-	document.addEventListener( "DOMContentLoaded", function(){
-    	document.removeEventListener( "DOMContentLoaded", arguments.callee, false);
-    	//setTimeout(function(){
-			domReady();
-		//}, 100);    
-}, false );
-
-// for the IE event model 
-} else if ( document.attachEvent ) {
-	// ensure firing before onload
-	document.attachEvent("onreadystatechange", function(){
-    	if ( document.readyState === "complete" ) {
-    		document.detachEvent( "onreadystatechange", arguments.callee );
-    		//setTimeout(function(){
-				domReady();
-			//}, 100);
-    	}
-  	})
-};
-
-
+}
